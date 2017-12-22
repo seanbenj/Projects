f0027a504b840953527e092ac3ef0f1e601346b6
seanbenj <seanbenj@gmail.com>
2017-12-22 00:35:06 -0800

    Coupon Validation with Ajax
    I created a mini project to then use my code to fix an issue with coupon validation on www.creditaccelerator.com

---
+++ AJAXCOUPONPRJ/AJAXCOUPONPRJ/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace AJAXCOUPONPRJ.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Boolean ValidateCoupon(string couponEntry)
+        {
+            if (couponEntry.Trim().Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AJAXCOUPONPRJ/AJAXCOUPONPRJ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AJAXCOUPONPRJ.MvcApplication" Language="C#" %>
